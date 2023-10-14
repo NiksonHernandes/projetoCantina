00a7399055bef9244374a1f05cd4ee124e6573bf
Nikson Hernandes <Nik.Hernandes14@hotmail.com>
2023-10-14 16:47:26 -0300

    Inicio modelagem Carrinho

---
+++ backend/cantina/src/main/java/com/cantina/cantina/domain/models/Carrinho.java
@@ -0,0 +1,38 @@
+package com.cantina.cantina.domain.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Carrinho {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Float valorTotal;
+    private String descricaoDaCompra;
+
+    //Lista de alimentos
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "carrinho_alimentos",
+            joinColumns = @JoinColumn(name = "carrinho_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "alimento_id", referencedColumnName = "id"))
+    private List<Alimento> alimentos = new ArrayList<>();
+
+    //Lista de bebidas
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "carrinho_bebidas",
+            joinColumns = @JoinColumn(name = "carrinho_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "bebida_id", referencedColumnName = "id"))
+    private List<Bebida> bebidas = new ArrayList<>();
+
+}
